23ae2df5312d26e6b734fe15f896610dec5b44d4
kyuung <mel1521@naver.com>
2021-12-12 14:07:02 +0900

    💩 Add alies

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import Home from './components/pages/user/UserHome';
+import UserHome from './components/pages/user/UserHome';
 
 const App = () => {
 	return (
 		<div>
 			Appjs
-			<Home test={'홈입니다'} />
+			<UserHome test={'홈입니다'} />
 		</div>
 	);
 };
