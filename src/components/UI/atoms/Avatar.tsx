3e6057cab70a3fca1e510287fdd9634eb4c5923f
kyuung <mel1521@naver.com>
2021-12-14 00:07:29 +0900

    ✅  Update avatar

--- src/components/UI/atoms/Avatar.tsx
+++ src/components/UI/atoms/Avatar.tsx
@@ -1,20 +1,29 @@
 import React from 'react';
+import styled from 'styled-components';
 
 interface AvatarProps {
 	nickname: string;
+	color: string;
 }
 
 const Avatar: React.FC<AvatarProps> = (props) => {
 	const { nickname } = props;
 	return (
 		<>
-			<div>{nickname.slice(0, 2)}</div>
+			<AvatarCircle>{nickname.slice(0, 2)}</AvatarCircle>
 		</>
 	);
 };
 
 Avatar.defaultProps = {
 	nickname: 'kyuung',
+	color: '#DDA211',
 };
+
+const AvatarCircle = styled.div`
+	background-color: ${(props) => props.color};
+	width: 36px;
+	height: 36px;
+`;
 
 export default Avatar;
