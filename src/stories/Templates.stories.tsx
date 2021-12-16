de72ccba2f56cbad761aaa32e6bd6c910ed29279
jinseoIT <69947833+jinseoIT@users.noreply.github.com>
2021-12-17 03:34:53 +0900

    :lipstick: Add templates overviewTemp

---
+++ src/stories/Templates.stories.tsx
@@ -0,0 +1,10 @@
+import OverviewTemp from '../components/templates/OverviewTemp';
+
+export default {
+	title: 'Templates',
+	component: OverviewTemp,
+};
+
+export const overviewTemp = () => {
+	return <OverviewTemp />;
+};
